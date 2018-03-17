bac1cd2b934dfd3f6d666d90795d57788e1db5c7
robelix <roland@robelix.com>
2018-03-17 21:11:12 +0100

    remove old isitopen-api

--- status-s.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-type: text/plain');
-    $str=file_get_contents('status.txt');
-    $arr=explode(',',$str);
-    $open=$arr[0];
-    $date=$arr[1]; //lastchange: upadate via file see explode/implode
-    echo $open;
-?>
